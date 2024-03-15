a652aa2b1bd09ed91e496a58fc8cae6eff33d96c
codewithash <munyaiashley99@gmail.com>
2024-03-16 00:58:48 +0200

    C++ Practice

---
+++ outputStatements/main.cpp
@@ -0,0 +1,24 @@
+// This program illustrates how output statements work.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    a = 65;
+    b = 78;
+
+    cout << 29 / 4 << endl;
+    cout << 3.0 / 2 << endl;
+    cout << "Hello there.\n";
+    cout << 7 << endl;
+    cout << 3 + 5 << endl;
+    cout << "3 + 5";
+    cout << endl;
+    cout << a << endl;
+    cout << "a" << endl;
+    cout << (a + 5) * 6 << endl;
+    cout << 2 * b << endl;
+    return 0;
+}
