cc9c6f04f4603c4db8a7ea8fc472c3e60e22da89
codewithash <134801667+codewithash-hub@users.noreply.github.com>
2024-04-08 23:22:17 +0200

    C++

---
+++ nestedLoops/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j;
+
+    for (i = 0; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+             printf("*");
+        }
+        printf("\n");
+    }
+
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+             printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
